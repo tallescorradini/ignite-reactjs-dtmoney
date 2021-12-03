3f98d4ed83ff1af712c056261b28f344694b284a
Talles Corradini <tallescorradini@gmail.com>
2021-12-02 22:26:24 -0300

    feat: add global font style properties

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -19,6 +19,15 @@ export const GlobalStyle = createGlobalStyle`
   box-sizing: border-box;
 }
 
+body, input, textarea,button{
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+}
+
+h1, h2, h3, h4, h5, h6, strong {
+  font-weight: 600;
+}
+
 html {
   @media (max-width:1080px){
     font-size: 93.75%
